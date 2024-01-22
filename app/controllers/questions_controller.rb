61e4968afe12632e84b2a7469ab4a21c670be605
Kai Tassie <argentiumkai@gmail.com>
2024-01-22 14:58:34 +0000

    Pushing finished rails stupid coaching

--- app/controllers/questions_controller.rb
+++ app/controllers/questions_controller.rb
@@ -4,8 +4,14 @@ class QuestionsController < ApplicationController
 
     def answer
         @question = params[:question]
+        if params[:question] == "i am going to work right now"
+            return @answer = ""
+        elsif params[:question].end_with?("?")
+            return @answer = "Silly question, get dressed and go to work!"
+        else
+            return @answer = "I don't care, get dressed and go to work!"
+        end
     end
-
 end
 
 
